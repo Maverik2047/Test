4d599390402a37d690f3703e7160b122c49c1f51
Viacheslav <maverik2047@yandex.ru>
2022-05-24 22:21:18 +0300

    second

---
+++ src/main/java/guru/qa/FirstTest.java
@@ -0,0 +1,13 @@
+package guru.qa;
+
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class FirstTest {
+    @Test
+
+public void firstTest(){
+        Assertions.assertTrue(3>2);
+    }
+}
